c0b5c1b3005c102b39a3eb8cbda1d4238d64b140
evgeny <afazhenya2001@gmail.com>
2023-08-01 22:59:55 +0300

    пустая страничка с профилем

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import SignUpPage, { SignUpPageRootURL } from '@pages/SignUp';
 import SignInPage, { SignInPageRootURL } from '@pages/SignIn';
+import MyProfilePage, { MyProfilePageRootURL } from '@pages/MyProfile';
 
 const root = document.getElementById('root');
 
 switch (window.location.pathname) {
-  case '/':
-    break;
   case SignInPageRootURL:
     root.innerHTML = SignInPage();
     break;
   case SignUpPageRootURL:
     root.innerHTML = SignUpPage();
+    break;
+  case MyProfilePageRootURL:
+    root.innerHTML = MyProfilePage();
+    break;
+  case '/':
     break;
 }
---
+++ src/pages/MyProfile/index.js
@@ -0,0 +1,7 @@
+import MyProfileTemplate from './myProfile.hbs';
+
+export const MyProfilePageRootURL = '/my-profile';
+
+export default () => {
+  return MyProfileTemplate();
+};
