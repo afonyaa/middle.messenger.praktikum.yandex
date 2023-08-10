34583b598e47b9afcd121ef952df9baac1fe3b4d
evgeny <afazhenya2001@gmail.com>
2023-08-10 23:41:23 +0300

    my-profile page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+import Handlebars from 'handlebars/runtime';
+import BaseButton from '@ui/BaseButton/baseButton.hbs';
+
 import SignUpPage, { SignUpPageRootURL } from '@pages/SignUp';
 import SignInPage, { SignInPageRootURL } from '@pages/SignIn';
 import MyProfilePage, { MyProfilePageRootURL } from '@pages/MyProfile';
+
+Handlebars.registerPartial('baseButton', BaseButton);
 
 const root = document.getElementById('root');
 
