27cf363b0cc302a7a320c9841ada6a41191556ea
evgeny <afazhenya2001@gmail.com>
2023-08-31 01:04:49 +0300

    fix lighthouse urls

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,12 @@
 module.exports = {
   ci: {
+    collect: {
+      url: [
+        'https://spontaneous-panda-71c00f.netlify.app/sign-in',
+        'https://spontaneous-panda-71c00f.netlify.app/sign-up',
+        'https://spontaneous-panda-71c00f.netlify.app/my-profile/preview',
+      ],
+    },
     upload: {
       target: 'temporary-public-storage',
     },
